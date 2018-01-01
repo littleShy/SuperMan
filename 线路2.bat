85490f5d874ba35f40d49818e7b485b7616a97e5
xiaoyl <signalyongli@163.com>
2018-01-01 14:06:27 +0800

    线路脚本

---
+++ 线路2.bat
@@ -0,0 +1,1 @@
+.\SuperMan.exe --net-type line2 --show-progress false --do-task-type all --max-error -1 --use-play-interval true
---
+++ 线路3.bat
@@ -0,0 +1,1 @@
+.\SuperMan.exe --net-type line3 --show-progress false --do-task-type all --max-error -1 --use-play-interval true
