67dcafe1debc517aa1b1d9787931fc524dbe200e
xiaoyl <signalyongli@163.com>
2018-01-01 15:24:17 +0800

    新增王体账号

--- ConstData.py
+++ ConstData.py
@@ -45,11 +45,34 @@ userAuthList = {
         'ZZR152011' : 'http://sdk.cferw.com/api.php?z=3286&appkey=528ab6a566564a96a0b7646586f3c017&deviceId=863970024695496&sw=1080&sh=1920&osver=17',
         'ZZR8030011' : 'http://sdk.cferw.com/api.php?z=3286&appkey=528ab6a566564a96a0b7646586f3c017&deviceId=865703023539151&sw=1080&sh=1920&osver=19',
         'ZZR283074' : 'http://sdk.cferw.com/api.php?z=3286&appkey=528ab6a566564a96a0b7646586f3c017&deviceId=866642024220404&sw=720&sh=1280&osver=19',
+    },
+    "WangTi-1":{
+        "XXR184054": "http://sdk.cferw.com/api.php?z=3286&appkey=528ab6a566564a96a0b7646586f3c017&deviceId=862528035655759&sw=480&sh=854&osver=23",
+        "XXR3090013": "http://sdk.cferw.com/api.php?z=3286&appkey=528ab6a566564a96a0b7646586f3c017&deviceId=862528035657078&sw=480&sh=854&osver=23",
+        "XXR715022": "http://sdk.cferw.com/api.php?z=3286&appkey=528ab6a566564a96a0b7646586f3c017&deviceId=351888063110632&sw=720&sh=1280&osver=18",
+        "XXR9017913": "http://sdk.cferw.com/api.php?z=3286&appkey=528ab6a566564a96a0b7646586f3c017&deviceId=868261025815340&sw=720&sh=1280&osver=22",
+        "XXR211057": "http://sdk.cferw.com/api.php?z=3286&appkey=528ab6a566564a96a0b7646586f3c017&deviceId=862350035092333&sw=720&sh=1280&osver=22",
+        "XXR7041512": "http://sdk.cferw.com/api.php?z=3286&appkey=528ab6a566564a96a0b7646586f3c017&deviceId=355060062857367&sw=720&sh=1280&osver=18",
+        "XXR210036": "http://sdk.cferw.com/api.php?z=3286&appkey=528ab6a566564a96a0b7646586f3c017&deviceId=862528036992789&sw=720&sh=1280&osver=23",
+        "XXR202616": "http://sdk.cferw.com/api.php?z=3286&appkey=528ab6a566564a96a0b7646586f3c017&deviceId=862528035655734&sw=480&sh=854&osver=23",
+        "XXR206202": "http://sdk.cferw.com/api.php?z=3286&appkey=528ab6a566564a96a0b7646586f3c017&deviceId=868261025890178&sw=720&sh=1280&osver=22",
+    },
+    "WangTi-2":{
+        "XXR1072034": "http://sdk.cferw.com/api.php?z=3286&appkey=528ab6a566564a96a0b7646586f3c017&deviceId=868261025816801&sw=720&sh=1280&osver=22",
+        "XXR1036614": "http://sdk.cferw.com/api.php?z=3286&appkey=528ab6a566564a96a0b7646586f3c017&deviceId=868261025815324&sw=720&sh=1280&osver=22",
+        "XXR66592": "http://sdk.cferw.com/api.php?z=3286&appkey=528ab6a566564a96a0b7646586f3c017&deviceId=355060064184950&sw=720&sh=1280&osver=18",
+        "XXR1050812": "http://sdk.cferw.com/api.php?z=3286&appkey=528ab6a566564a96a0b7646586f3c017&deviceId=862528035655379&sw=480&sh=854&osver=23",
+        "XXR103063": "http://sdk.cferw.com/api.php?z=3286&appkey=528ab6a566564a96a0b7646586f3c017&deviceId=351888069992496&sw=720&sh=1280&osver=18",
+        "XXR108256": "http://sdk.cferw.com/api.php?z=3286&appkey=528ab6a566564a96a0b7646586f3c017&deviceId=355060060609786&sw=720&sh=1280&osver=18",
+        "XXR3020016": "http://sdk.cferw.com/api.php?z=3286&appkey=528ab6a566564a96a0b7646586f3c017&deviceId=351888067709207&sw=720&sh=1280&osver=18",
+        "XXR125452": "http://sdk.cferw.com/api.php?z=3286&appkey=528ab6a566564a96a0b7646586f3c017&deviceId=355060061154824&sw=720&sh=1280&osver=18",
+        "XXR262026": "http://sdk.cferw.com/api.php?z=3286&appkey=528ab6a566564a96a0b7646586f3c017&deviceId=351888065323563&sw=720&sh=1280&osver=18",
+    },
+    "WangTi-3":{
+        "XXR192029": "http://sdk.cferw.com/api.php?z=3286&appkey=528ab6a566564a96a0b7646586f3c017&deviceId=355060061885450&sw=720&sh=1280&osver=18",
+        "XXR265021": "http://sdk.cferw.com/api.php?z=3286&appkey=528ab6a566564a96a0b7646586f3c017&deviceId=358858050569069&sw=720&sh=1280&osver=18",
     }
 }
-# money=1.5&username=ZZR100022&yuliu2=1&taskname=%E5%AE%89%E6%99%BA%E5%B8%82%E5%9C%BA3&taskid=334
-# specialTaskTypeUser = [              'ZZR711001',            'ZZR197745','ZZR1083072','ZZR1094018',            'ZZR212016']
-specialTaskTypeUser = ['ZZR129152','ZZR711001','ZZR112004','ZZR197745','ZZR1083072','ZZR1094018','ZZR100022','ZZR212016']
 
 comment = {
     'ZZR202038':{'Tag':'程1','personLiable':'程'},
@@ -86,7 +109,29 @@ comment = {
     'ZZR8030011':{'Tag':'肖1','personLiable':'程'},
     'ZZR284058':{'Tag':'肖2','personLiable':'程'},
     'ZZR283074':{'Tag':'肖3','personLiable':'程'},
-    'ZZR174517':{'Tag':'肖5','personLiable':'程'}
+    'ZZR174517':{'Tag':'肖5','personLiable':'程'},
+
+    # 王体账号
+    "XXR184054":{'Tag':'王体', 'personLiable':'王体'},
+    "XXR3090013":{'Tag':'王体', 'personLiable':'王体'},
+    "XXR715022":{'Tag':'王体', 'personLiable':'王体'},
+    "XXR9017913":{'Tag':'王体', 'personLiable':'王体'},
+    "XXR211057":{'Tag':'王体', 'personLiable':'王体'},
+    "XXR7041512":{'Tag':'王体', 'personLiable':'王体'},
+    "XXR210036":{'Tag':'王体', 'personLiable':'王体'},
+    "XXR202616":{'Tag':'王体', 'personLiable':'王体'},
+    "XXR206202":{'Tag':'王体', 'personLiable':'王体'},
+    "XXR1072034":{'Tag':'王体', 'personLiable':'王体'},
+    "XXR1036614":{'Tag':'王体', 'personLiable':'王体'},
+    "XXR66592":{'Tag':'王体', 'personLiable':'王体'},
+    "XXR1050812":{'Tag':'王体', 'personLiable':'王体'},
+    "XXR103063":{'Tag':'王体', 'personLiable':'王体'},
+    "XXR108256":{'Tag':'王体', 'personLiable':'王体'},
+    "XXR3020016":{'Tag':'王体', 'personLiable':'王体'},
+    "XXR125452":{'Tag':'王体', 'personLiable':'王体'},
+    "XXR262026":{'Tag':'王体', 'personLiable':'王体'},
+    "XXR192029":{'Tag':'王体', 'personLiable':'王体'},
+    "XXR265021":{'Tag':'王体', 'personLiable':'王体'}
 }
 commonUrls = [
     {
---
+++ WangTi-3.bat
@@ -0,0 +1,1 @@
+.\SuperMan.exe --net-type WangTi-3 --show-progress false --do-task-type all --max-error -1 --use-play-interval true
